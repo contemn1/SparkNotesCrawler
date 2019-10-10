db877df1c1c3c22a92a28dea9e838d11859f23c7
xunjie zhu <xunjiez614218@gmail.com>
2019-10-10 17:11:51 -0400

    add file io_util.py

---
+++ io_util.py
@@ -0,0 +1,24 @@
+import io
+import logging
+import sys
+
+
+def output_iterator(file_path, output_list, process=lambda x: x):
+    try:
+        with io.open(file_path, mode="w+", encoding="utf-8") as file:
+            for line in output_list:
+                file.write(process(line) + "\n")
+    except IOError as error:
+        logging.error("Failed to open file {0}".format(error))
+        sys.exit(1)
+
+
+def read_file(file_path, encoding="utf-8", preprocess=lambda x: x):
+    try:
+        with io.open(file_path, encoding=encoding) as file:
+            for sentence in file.readlines():
+                yield (preprocess(sentence))
+
+    except IOError as err:
+        logging.error("Failed to open file {0}".format(err))
+        sys.exit(1)
