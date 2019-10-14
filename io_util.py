d32a3995dd8840a6806a262a71b66ba1bed4a055
contemn1 <xunjiez614218@gmail.com>
2019-10-14 06:09:11 -0400

    add crawl summary methods

--- io_util.py
+++ io_util.py
@@ -22,3 +22,13 @@ def read_file(file_path, encoding="utf-8", preprocess=lambda x: x):
     except IOError as err:
         logging.error("Failed to open file {0}".format(err))
         sys.exit(1)
+
+if __name__ == '__main__':
+    index = 0
+    input_path = "/home/zxj/Data/SparkNotes/urls.txt"
+    output_path = "/home/zxj/Data/SparkNotes/url_parts/url_part_{0}.txt"
+    url_string_list = list(read_file(input_path, preprocess=lambda x: x.strip()))
+    index = 0
+    for i in range(0, len(url_string_list), 30):
+        output_iterator(output_path.format(index), url_string_list[i: i + 30])
+        index += 1
--- url_crawler.py
+++ url_crawler.py
@@ -1,15 +1,19 @@
-import os
+import asyncio
+import json
+from typing import Dict
 from typing import List
 
-from io_util import output_iterator, read_file
-import json
 import aiohttp
-import asyncio
 from bs4 import BeautifulSoup
+
+from io_util import read_file, output_iterator
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Safari/605.1.15"}
 
 
 async def fetch(session: aiohttp.ClientSession, url: str) -> str:
-    await asyncio.sleep(1)
+    await asyncio.sleep(4)
     async with session.get(url) as response:
         return await response.text()
 
@@ -32,33 +36,65 @@ def parse_home_page(html_doc) -> List[str]:
     return result_list
 
 
+async def get_url_link(session: aiohttp.ClientSession, json_dict: Dict[str, str]):
+    html_doc = await fetch(session, json_dict['url'])
+    result_list = parse_url_page(html_doc)
+    json_dict['chapters_url'] = result_list
+
+
 def parse_url_page(html_doc):
     soup = BeautifulSoup(html_doc, 'html.parser')
     res = soup.find(name='li',
-                    attrs={'class': "category"}).parent
+                    attrs={'class': "category"})
+    if not res or not res.parent:
+        return ''
+
+    res = res.parent
     url_class_name = 'section-list__link indented titlepage_content1'
     res_list = res.find_all(name='li',
-                            attrs={'class': url_class_name})
+                            attrs={'class': url_class_nammissing_urls_new.txte})
     res_list = [ele.find('a') for ele in res_list]
     res_list = [{'name': res.contents[0], 'url': res.attrs['href']}
                 for res in res_list]
     return res_list
 
 
-async def main(url_dict_list):
-    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Safari/605.1.15"}
+async def get_url_link_list(url_dict_list, get_link=get_url_link):
     async with aiohttp.ClientSession(headers=headers) as session:
-        done, pending = await asyncio.wait([fetch(session, ele['url']) for ele in url_dict_list])
-        return done
+        return await asyncio.wait([get_link(session, ele) for ele in url_dict_list])
+
+
+def parse_plot_overview(html_doc):
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    overview_div = soup.find("div", attrs={"id": "plotoverview"})
+    res = (ele.string.strip() for ele in overview_div.children)
+    return "\n".join([text for text in res if text])
+
+
+async def get_summary(session, json_dict):
+    url_string = json_dict.pop('url', '')
+    if url_string:
+        html_doc = await fetch(session, url_string)
+        json_dict['summary'] = parse_plot_overview(html_doc)
+
+
+def main():
+    input_path = "/home/zxj/Data/SparkNotes/url_parts/url_part_{0}.txt"
+    root_url = "https://www.sparknotes.com"
+    output_path = "/home/zxj/Data/SparkNotes/chapters_url_parts/chapters_url_part_{0}.txt"
+    loop = asyncio.get_event_loop()
+    input_list = list(
+        read_file("/home/zxj/Data/SparkNotes/missing_urls.txt", preprocess=lambda x: json.loads(x.strip())))
+    for ele in input_list:
+        print(ele)
+    loop.run_until_complete(get_url_link_list(input_list))
+
+    output_iterator("/home/zxj/Data/SparkNotes/missing_urls_new.txt", input_list, process=lambda x: json.dumps(x))
+
 
 if __name__ == '__main__':
-    input_path = "/Users/zxj/Google 云端硬盘/SparkNotes/urls_test.txt"
-    root_url = "https://www.sparknotes.com"
-    input_list = list(read_file(input_path, preprocess=lambda x: json.loads(x.strip())))
-    for ele in input_list:
-        ele['url'] = root_url + ele['url']
-
+    input_path = "/home/zxj/Data/SparkNotes/chapter_urls.txt"
+    input_list = read_file(input_path, preprocess=lambda x: json.loads(x.strip()))
     loop = asyncio.get_event_loop()
-    result_list = loop.run_until_complete(main(input_list))
-    for ele in result_list:
-        print(ele.result())
+    results = loop.run_until_complete(get_url_link_list(input_list, get_link=get_summary))
+    output_iterator("/home/zxj/Data/SparkNotes/book_summaries", input_list, process=lambda x: json.dumps(x))
